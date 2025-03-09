f67e56d4e434d8a9f64dba4c8dc6651cb8ca5e55
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-03-09 17:23:51 -0300

    Define base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    FooterComponent,
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule,
