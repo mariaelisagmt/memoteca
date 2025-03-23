252084990eba42eaf28ba6776a1200fa5727277e
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-03-23 18:42:25 -0300

    add ngClass

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -11,12 +11,32 @@ export class ListarPensamentoComponent implements OnInit {
     {
       conteudo: 'Teste de componentes',
       autoria: 'Angular',
-      modelo: 'modelo3'
+      modelo: 'modelo1'
     },
     {
       conteudo: 'Comunicacao entre componentes',
       autoria: 'Angualar',
-      modelo: 'modelo4'
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Comunicacao',
+      autoria: 'Angualar',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem Ipsum is simply dummy text of the pr.',
+      autoria: 'Angualar',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem Ipsum is simply dummy text of the printing m.',
+      autoria: 'Angualar',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      autoria: 'Angualar',
+      modelo: 'modelo3'
     }
   ];
 
