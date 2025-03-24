df467473d7e33804ea7dcad1acf79ddf60a735b4
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-03-24 08:39:17 -0300

    add function delete

--- src/app/components/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
+++ src/app/components/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -1,5 +1,7 @@
+import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
-
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-excluir-pensamento',
   templateUrl: './excluir-pensamento.component.html',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExcluirPensamentoComponent implements OnInit {
 
-  constructor() { }
+  pensamento: Pensamento = {
+    id: '',
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service.buscarPorId(id!).subscribe((pensamento) => {
+      this.pensamento = pensamento;
+    })
+  }
+
+  excluirPensamento() {
+    if(this.pensamento.id != ''){
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      })
+    }
+  }
+
+  cancelar(){
+    this.router.navigate(['/listarPensamento']);
   }
 
 }
