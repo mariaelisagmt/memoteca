639b9b9ff4040a2a969dcfb1be3250a338a9a080
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-04-01 08:23:25 -0300

    add get pagination

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -10,11 +10,12 @@ import { PensamentoService } from '../pensamento.service';
 export class ListarPensamentoComponent implements OnInit {
 
   listarPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1;
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe(listaPensamentos => {
+    this.service.listar(this.paginaAtual).subscribe(listaPensamentos => {
       this.listarPensamentos = listaPensamentos;
     });
   }
